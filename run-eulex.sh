ca8136accec376eed16a6a11b6ef9cacb54cc07f
David Vázquez <davazp@gmail.com>
2011-11-14 12:53:39 +0000

    Disable network in Qemu to speed up the boot

--- run-eulex.sh
+++ run-eulex.sh
@@ -2,6 +2,6 @@
 
 # TODO: Add checks for qemu installation
 KERNEL=eulex
-qemu -soundhw pcspk -s -serial stdio -kernel $KERNEL
+qemu -soundhw pcspk -s -serial stdio -net none -kernel $KERNEL
 
 # run-eulex.sh ends here
