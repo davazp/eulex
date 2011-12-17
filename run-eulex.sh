883c84752a1d0db4942e3f89594c649a80c49f14
David Vázquez <davazp@gmail.com>
2011-12-17 14:30:37 +0000

    Pass arguments through to qemu

--- run-eulex.sh
+++ run-eulex.sh
@@ -2,6 +2,6 @@
 
 # TODO: Add checks for qemu installation
 KERNEL=eulex.core
-qemu -soundhw pcspk -s -serial stdio -net none -kernel $KERNEL
+qemu $@ -soundhw pcspk -s -serial stdio -net none -kernel $KERNEL
 
 # run-eulex.sh ends here
