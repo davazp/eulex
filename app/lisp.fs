948946a773b7e48d9750ec533e7170964a5b8583
David Vázquez <davazp@gmail.com>
2012-03-25 12:21:41 +0100

    Add TODO: garbage collection

--- app/lisp.fs
+++ app/lisp.fs
@@ -17,6 +17,8 @@
 \ You should have received a copy of the GNU General Public License
 \ along with Eulex.  If not, see <http://www.gnu.org/licenses/>.
 
+\ TODO: Implement a basic GC!
+
 vocabulary lisp
 get-current
 also eulex
