46136b33e41774d761439f578e75618debb2a27f
David Vázquez <davazp@gmail.com>
2012-04-01 19:26:44 +0100

    SETQ, LAMBDA, DEFUN and DEFMACRO

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,1 +1,20 @@
+;;; (defmacro name args body)
+;;; =>
+;;; (fset name `(macro lambda ,args ,body))
+(fset 'defmacro
+      '(macro lambda (name args expr)
+        (list 'fset (list 'quote name)
+         (list 'quote
+          (list 'macro 'lambda args expr)))))
 
+(defmacro setq (symbol value)
+  (list 'set (list 'quote symbol) value))
+
+(defmacro lambda (args body)
+  (list 'quote (list 'lambda args body)))
+
+(defmacro defun (name args body)
+  (list 'progn
+        (list 'fset (list 'quote name)
+              (list 'quote (list 'lambda args body)))
+        (list 'quote name)))
