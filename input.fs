3d59ff791620df0b324c3c0a40dbd8396ee852bb
David Vázquez <davazp@gmail.com>
2012-04-24 19:03:54 +0100

    LOAD word to execute blocks

--- input.fs
+++ input.fs
@@ -63,8 +63,7 @@ CREATE TIB video-width allot
         true
     else
         false
-    endif
-;
+    endif ;
 
 :noname
     refill-silent? @ not if
@@ -103,5 +102,19 @@ CREATE TIB video-width allot
     \ Restore the input source from the control stack
     r> dup begin dup 0 > while 1- r> -rot repeat drop
     restore-input ;
+
+
+\ Blocks
+variable blk?
+variable blk
+
+: block\ begin >IN @ 64 mod while parse-char drop repeat ; immediate
+: \ blk? @ if postpone block\ else postpone \ then ; immediate
+
+\ TODO: Not to use buffers to evaluate the blocks
+: load ( u -- )
+    blk? on
+    blk @ swap dup blk ! block 1024 evaluate blk !
+    blk? off ;
 
 \ input.fs ends here
